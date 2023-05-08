e3ab31bdf5998c35f69d9c20fe6ecd41da9dd048
marco-san-juan <marco.e.sanjuan@gmail.com>
2023-05-08 17:17:57 +0800

    User Seeders

--- src/main/java/com/errand/services/impl/ServiceProviderServiceImpl.java
+++ src/main/java/com/errand/services/impl/ServiceProviderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.errand.services.impl;
 
+import com.errand.dto.ServiceProviderForDisplayDto;
 import com.errand.dto.ServiceProviderForUpdateDto;
 import com.errand.mapper.ServiceProviderMapper;
 import com.errand.models.ServiceProvider;
@@ -8,7 +9,9 @@ import com.errand.services.ServiceProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceProviderServiceImpl implements ServiceProviderService {
@@ -24,5 +27,13 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
 
         Optional<ServiceProvider> serviceProviderOptional = serviceProviderRepository.findById(id);
         return serviceProviderOptional.map(serviceProviderMapper::toServiceProviderForUpdateDto);
+    }
+
+    @Override
+    public List<ServiceProviderForDisplayDto> getAllServiceProvider() {
+        return serviceProviderRepository.findAll()
+                .stream()
+                .map(serviceProviderMapper::toServiceProviderForDisplayDto)
+                .collect(Collectors.toList());
     }
 }
