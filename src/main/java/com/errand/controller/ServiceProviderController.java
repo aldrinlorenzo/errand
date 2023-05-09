88d8273f7592c86d7f027b702f0906c6057bc13a
marco-san-juan <marco.e.sanjuan@gmail.com>
2023-05-09 13:49:37 +0800

    ERD-30

--- src/main/java/com/errand/controller/ServiceProviderController.java
+++ src/main/java/com/errand/controller/ServiceProviderController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,21 +31,27 @@ public class ServiceProviderController {
     @GetMapping("/{serviceProviderId}/dashboard")
     public String getServiceProviderDetails(@PathVariable Long serviceProviderId, Model model) {
 
-        // Get service provider by ID
-        ServiceProviderForUpdateDto serviceProviderDto = serviceProviderService.getServiceProviderById(serviceProviderId).orElseThrow(() -> new IllegalArgumentException("Invalid service provider ID: " + serviceProviderId));
-        model.addAttribute("serviceProvider", serviceProviderDto);
+        ServiceProviderForDisplayDto serviceProviderForDisplayDto = serviceProviderService.getCurrentServiceProvider();
+        model.addAttribute("serviceProvider", serviceProviderForDisplayDto );
+//        List<PendingTaskDto> pendingTasks = taskService.getPendingTask();
+//        model.addAttribute("pendingTasks", pendingTasks);
 
-        // Get pending tasks for service provider
-        List<PendingTaskDto> pendingTasks = taskService.getPendingTask();
-        model.addAttribute("pendingTasks", pendingTasks);
-
-        return "serviceProviderDetails";
+        return "serviceprovider-dashboard";
     }
-//    @GetMapping("/{serviceProviderId}/dashboard/findAll")
-//    public ResponseEntity<List<ServiceProviderForDisplayDto>> findAll() {
-//        List<ServiceProviderForDisplayDto> serviceProviders = serviceProviderService.getAllServiceProvider();
-//        return ResponseEntity.ok(serviceProviders);
-//    }
+    @PutMapping("/{serviceProviderId}/dashboard/")
+    public String  updateServiceProviderDetails(@PathVariable Long serviceProviderId, @RequestBody ServiceProviderForUpdateDto serviceProviderDto, Model model) {
+        boolean isUpdated = serviceProviderService.updateServiceProviderDetails(serviceProviderDto,serviceProviderId);
+
+        if (isUpdated) {
+            model.addAttribute("serviceProvider", serviceProviderDto);
+            model.addAttribute("serviceProviderId", serviceProviderId);
+            return "serviceprovider-dashboard";
+        } else {
+            model.addAttribute("errorMessage", "Failed to update service provider with ID: " + serviceProviderId);
+            return "error";
+        }
+
+    }
 
 }
 
