3e07de276b1cb280e6cd93dc2a461f04eaa0dab6
“aldrinlorenzo” <aldrinlorenzo.work@gmail.com>
2023-05-17 17:56:40 +0800

    Modified code for saving image

--- src/main/java/com/errand/controller/ClientController.java
+++ src/main/java/com/errand/controller/ClientController.java
@@ -78,16 +78,10 @@ public class ClientController {
 
     @PostMapping("/profile/upload")
     public String uploadImage(Model model, @RequestParam("file") MultipartFile file) {
-        String message = "";
-
         try {
             fileStorageService.saveClientImage(file, clientService.getCurrentClient());
-
-            message = "Uploaded the image successfully: " + file.getOriginalFilename();
-            model.addAttribute("message", message);
         } catch (Exception e) {
-            message = "Could not upload the image: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
-            model.addAttribute("message", message);
+            e.printStackTrace();
         }
 
         return "redirect:/client/profile";
--- src/main/java/com/errand/controller/ServiceProviderController.java
+++ src/main/java/com/errand/controller/ServiceProviderController.java
@@ -93,16 +93,11 @@ public class ServiceProviderController {
 
     @PostMapping("/profile/upload")
     public String uploadImage(Model model, @RequestParam("file") MultipartFile file) {
-        String message = "";
 
         try {
             fileStorageService.saveServiceProviderImage(file, serviceProviderService.getCurrentServiceProvider());
-
-            message = "Uploaded the image successfully: " + file.getOriginalFilename();
-            model.addAttribute("message", message);
         } catch (Exception e) {
-            message = "Could not upload the image: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
-            model.addAttribute("message", message);
+            e.printStackTrace();
         }
 
         return "redirect:/serviceProvider/profile";
